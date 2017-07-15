2a8453bfff6a90639da53e34f4887754a5f1b39c
Jeremy Burns <j@jaburns.net>
2017-07-15 12:38:25 -0600

    Added shooting, new level structur

--- Assets/Scripts/BloodTrigger.cs
+++ Assets/Scripts/BloodTrigger.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BloodTrigger : MonoBehaviour {
+public class BloodTrigger : MonoBehaviour 
+{
+    void OnTriggerEnter(Collider x)
+    {
+        x.SendMessage("OnEnterBlood", SendMessageOptions.DontRequireReceiver);
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerExit(Collider x)
+    {
+        x.SendMessage("OnExitBlood", SendMessageOptions.DontRequireReceiver);
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,81 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
-    [SerializeField] float _moveForce = 10.0f;
-    [SerializeField] float _dragCoeff = 10.0f;
-    [SerializeField] float _jumpImpulse = 5.0f;
+    [Serializable]
+    struct MovementParams {
+        public float MoveForce;
+        public float DragCoeff;
+        public float JumpImpulse;
+    }
+
+    [SerializeField] MovementParams _bloodMovement;
+    [SerializeField] MovementParams _airMovement;
+    [SerializeField] int _groundTime;
+
+    MovementParams _curMovement;
     Rigidbody _rb;
+    int _grounded;
+    bool _pressedSpace = false;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _curMovement = _airMovement;
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            _rb.AddForce(_jumpImpulse * Vector3.up, ForceMode.Impulse);
-        }
+        if(_rb.IsSleeping()) _rb.WakeUp();
 
-        if (Input.GetKey(KeyCode.W)) _rb.AddForce(_moveForce * Vector3.forward);
-        if (Input.GetKey(KeyCode.S)) _rb.AddForce(_moveForce * Vector3.back);
-        if (Input.GetKey(KeyCode.A)) _rb.AddForce(_moveForce * Vector3.left);
-        if (Input.GetKey(KeyCode.D)) _rb.AddForce(_moveForce * Vector3.right);
+        Debug.Log(_grounded);
+
+        if (_grounded > 0) {
+            if (Input.GetKey(KeyCode.Space) && !_pressedSpace) {
+                _rb.AddForce(_curMovement.JumpImpulse * Vector3.up, ForceMode.Impulse);
+                _grounded = 0;
+            }
+            _grounded--;
+        }
+        _pressedSpace = Input.GetKey(KeyCode.Space);
+
+        if (Input.GetKey(KeyCode.W)) _rb.AddForce(_curMovement.MoveForce * Vector3.forward);
+        if (Input.GetKey(KeyCode.S)) _rb.AddForce(_curMovement.MoveForce * Vector3.back);
+        if (Input.GetKey(KeyCode.A)) _rb.AddForce(_curMovement.MoveForce * Vector3.left);
+        if (Input.GetKey(KeyCode.D)) _rb.AddForce(_curMovement.MoveForce * Vector3.right);
 
         var v_xz = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
 
-        _rb.AddForce(_dragCoeff * -v_xz);
+        _rb.AddForce(_curMovement.DragCoeff * -v_xz);
+
+
+        Plane playerPlane = new Plane(Vector3.up, _rb.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float rayDistance;
+        if (playerPlane.Raycast(ray, out rayDistance)) {
+            // _boj.transform.position = ray.GetPoint(rayDistance);
+        }
+    }
+
+    void OnEnterBlood()
+    {
+        _curMovement = _bloodMovement;
+    }
+
+    void OnExitBlood()
+    {
+        _curMovement = _airMovement;
+    }
+
+    void OnCollisionStay(Collision c)
+    {
+        foreach(var n in c.contacts) {
+            if (n.normal.y > 0.5f) {
+                _grounded = _groundTime;
+            }
+        }
     }
 }
