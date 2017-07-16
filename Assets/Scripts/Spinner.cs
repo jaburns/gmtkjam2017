9edc1ae24dacf0a450701ab731120775e3fa72a4
Jeremy Burns <j@jaburns.net>
2017-07-15 23:38:19 -0600

    Improved intro

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour 
+{
+	public float speed = 1.0f;
+
+	void Update()
+	{
+		transform.rotation = Quaternion.Euler(0, 0, Time.time * speed);
+	}
+}
